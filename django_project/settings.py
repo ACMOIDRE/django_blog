f0d741b1a2b594991d293458c0517a2193aed759
amir sohail <9902699141a@gmail.com>
2021-10-11 10:42:27 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2(dc2z7+9dmx2^1oy&wwz3bhkprxiny_a4%4wf!o8qcm3@hn-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
